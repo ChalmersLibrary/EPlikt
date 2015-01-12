c176734750faa3ee17e65a9c2396c9c05aa58b3a
jokermanse <urban.andersson@chalmers.se>
2015-01-12 14:33:46 +0100

    Added custom ns

--- EPlikt/Controllers/EPliktController.cs
+++ EPlikt/Controllers/EPliktController.cs
@@ -6,8 +6,10 @@ using System.Net.Http;
 using System.Web.Http;
 using System.ComponentModel.DataAnnotations;
 using EPlikt.Models;
+using Newtonsoft.Json;
 using System.ServiceModel.Syndication;
 using System.Xml;
+using System.Xml.Linq;
 using System.IO;
 using System.Text;
 
@@ -28,10 +30,28 @@ namespace EPlikt.Controllers
                 Description = new TextSyndicationContent("Denna feed innehåller data från Chalmers som berörs utav den nya E-Pliktslagen.")
             };
 
+            
+            // Create a new  XmlDocument in order to modify the root element
+            // http://stackoverflow.com/questions/14397392/overwriting-root-element-in-syndicationfeed-adding-namespaces-to-root-element
+            var rssFeedFormatter = new Rss20FeedFormatter(feed);
+            var xmlDoc = new XmlDocument();
+
+            // Write the RSS formatted feed directly into the xml doc
+            using (var xw = xmlDoc.CreateNavigator().AppendChild())
+            {
+                rssFeedFormatter.WriteTo(xw);
+            }
+
+            // Add custom namespace(s)
+            xmlDoc.DocumentElement.SetAttribute("xmlns:georss", "http://www.georss.org/georss");
+            xmlDoc.DocumentElement.SetAttribute("xmlns:media", "http://search.yahoo.com/mrss/");
+            xmlDoc.DocumentElement.SetAttribute("xmlns:dcterms", "http://purl.org/dc/terms/");
+
             var output = new StringWriter();
             var writer = new XmlTextWriter(output);
 
-            new Rss20FeedFormatter(feed).WriteTo(writer);
+            //new Rss20FeedFormatter(feed).WriteTo(writer);
+            xmlDoc.WriteTo(writer);
 
             var res = Request.CreateResponse(HttpStatusCode.OK);
             res.Content = new StringContent(output.ToString(), Encoding.UTF8, "application/rss+xml");
@@ -43,6 +63,9 @@ namespace EPlikt.Controllers
         {
             var ret = new List<SyndicationItem>();
 
+            // Retrieve items from Solr and return as feed items
+
+
             ret.Add(new SyndicationItem("TestTitle1", "TestContent1", new Uri("http://www.google.com")));
             ret.Add(new SyndicationItem("TestTitle2", "TestContent2", new Uri("http://www.google.com")));
             ret.Add(new SyndicationItem("TestTitle3", "TestContent3", new Uri("http://www.google.com")));
@@ -50,4 +73,6 @@ namespace EPlikt.Controllers
             return ret;
         }
     }
+
+    
 }
