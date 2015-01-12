f1e62807ae48dc639980a4e4b7dcdde80ac202c3
Lars Andersson <lars.j.andersson@chalmers.se>
2015-01-12 13:07:16 +0100

    Added test syndication.

--- EPlikt/Controllers/EPliktController.cs
+++ EPlikt/Controllers/EPliktController.cs
@@ -6,6 +6,10 @@ using System.Net.Http;
 using System.Web.Http;
 using System.ComponentModel.DataAnnotations;
 using EPlikt.Models;
+using System.ServiceModel.Syndication;
+using System.Xml;
+using System.IO;
+using System.Text;
 
 namespace EPlikt.Controllers
 {
@@ -16,14 +20,34 @@ namespace EPlikt.Controllers
         /// </summary>
         /// <returns>The feed.</returns>
         [HttpGet]
-        public ResultResponse Feed()
+        public HttpResponseMessage Feed()
         {
-            var res = new ResultResponse();
+            var feed = new SyndicationFeed(GetSyndicationItems())
+            {
+                Title = new TextSyndicationContent("Innehåll på Chalmers"),
+                Description = new TextSyndicationContent("Denna feed innehåller data från Chalmers som berörs utav den nya E-Pliktslagen.")
+            };
 
-            res.Success = true;
-            res.Message = "TODO!";
+            var output = new StringWriter();
+            var writer = new XmlTextWriter(output);
+
+            new Rss20FeedFormatter(feed).WriteTo(writer);
+
+            var res = Request.CreateResponse(HttpStatusCode.OK);
+            res.Content = new StringContent(output.ToString(), Encoding.UTF8, "application/rss+xml");
 
             return res;
+        }
+
+        private static List<SyndicationItem> GetSyndicationItems()
+        {
+            var ret = new List<SyndicationItem>();
+
+            ret.Add(new SyndicationItem("TestTitle1", "TestContent1", new Uri("http://www.google.com")));
+            ret.Add(new SyndicationItem("TestTitle2", "TestContent2", new Uri("http://www.google.com")));
+            ret.Add(new SyndicationItem("TestTitle3", "TestContent3", new Uri("http://www.google.com")));
+
+            return ret;
         }
     }
 }
