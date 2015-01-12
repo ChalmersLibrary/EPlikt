9f96c0680d554640298d881504386e0f2694f901
Lars Andersson <lars.j.andersson@chalmers.se>
2015-01-12 12:43:46 +0100

    Added NOP web api at /api/eplikt/feed.

---
+++ EPlikt/Controllers/EPliktController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.ComponentModel.DataAnnotations;
+using EPlikt.Models;
+
+namespace EPlikt.Controllers
+{
+    public class EPliktController : ApiController
+    {
+        /// <summary>
+        /// Get the main feed.
+        /// </summary>
+        /// <returns>The feed.</returns>
+        [HttpGet]
+        public ResultResponse Feed()
+        {
+            var res = new ResultResponse();
+
+            res.Success = true;
+            res.Message = "TODO!";
+
+            return res;
+        }
+    }
+}
---
+++ EPlikt/Models/ResultResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EPlikt.Models
+{
+    /// <summary>
+    /// General Response Object to use in CRUD communication
+    /// </summary>
+    public class ResultResponse
+    {
+        [Required]
+        public bool Success { get; set; }
+        public string Message { get; set; }
+    }
+}
