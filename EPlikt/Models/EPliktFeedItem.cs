7eec3d8c2a66419d4b06676de18696a4f161a604
jokermanse <urban.andersson@chalmers.se>
2015-01-13 15:33:29 +0100

    Added EPliktFeedItem

--- EPlikt/Models/EPliktFeedItem.cs
+++ EPlikt/Models/EPliktFeedItem.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,7 +8,21 @@ namespace EPlikt.Models
 {
     public class EPliktFeedItem
     {
+        public string Guid { get; set; }
         public string Title { get; set; }
         public string Abstract { get; set; }
+        public string Link { get; set; }
+        public string PubDate { get; set; }
+        public string Publisher { get; set; }
+        public string AccessRights { get; set; }
+        public string License { get; set; }
+        public string Format { get; set; }
+        public string ContentType { get; set; }
+        public string References { get; set; }
+        public string MD5 { get; set; }
+        public string Keywords { get; set; }
+        public string Category { get; set; }
+        public string Copyright { get; set; }
+        public List<string> Creator { get; set; }
     }
 }
